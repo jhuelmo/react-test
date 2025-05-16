acd40551a61fcc08c13332ca05774854c922cdf8
Josu Huelmo <josu.huelmo@gmail.com>
2025-05-16 19:36:54 +0200

    Added first styling of the carousel

--- src/components/UI/Carousel/carousel.tsx
+++ src/components/UI/Carousel/carousel.tsx
@@ -4,22 +4,25 @@ import room from "@/assets/slider/room.jpg";
 import tower from "@/assets/slider/tower.jpg";
 import "./carousel.css";
 
+//List for demo purpose
 const slides = [
-    { id: 1, content: angli },
-    { id: 2, content: room },
+    { id: 1, content: room },
+    { id: 2, content: angli },
     { id: 3, content: tower },
 ];
 
 export default function Carousel() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
     return (
         <div className="carousel" aria-label="Image carousel">
-            <div className="carousel-slides">
+            <ul className="carousel-slides">
                 {slides.map((slide, index) => (
-                    <div key={slide.id}>
-                        <img src={slide.content} />
-                    </div>
+                    <li key={slide.id} className="carousel-slide">
+                        <img src={slide.content} className="carousel-image" />
+                    </li>
                 ))}
-            </div>
+            </ul>
         </div>
     );
 }
