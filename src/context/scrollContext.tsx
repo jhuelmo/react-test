8347648166b410f6bbbe2e012a3e4c740107a717
Josu Huelmo <josu.huelmo@gmail.com>
2025-05-16 21:30:26 +0200

    Context added for scroll purpose

---
+++ src/context/scrollContext.tsx
@@ -0,0 +1,42 @@
+import React, {
+    createContext,
+    useContext,
+    useRef,
+    type ReactNode,
+    type RefObject,
+} from "react";
+
+type ScrollContextType = {
+    section1Ref: RefObject<HTMLElement | null>;
+    section2Ref: RefObject<HTMLElement | null>;
+    section3Ref: RefObject<HTMLElement | null>;
+    scrollTo: (ref: RefObject<HTMLElement | null>) => void;
+};
+
+const ScrollContext = createContext<ScrollContextType | undefined>(undefined);
+
+export const ScrollProvider = ({ children }: { children: ReactNode }) => {
+    const section1Ref = useRef<HTMLElement>(null);
+    const section2Ref = useRef<HTMLElement>(null);
+    const section3Ref = useRef<HTMLElement>(null);
+
+    const scrollTo = (ref: RefObject<HTMLElement | null>) => {
+        ref.current?.scrollIntoView();
+    };
+
+    return (
+        <ScrollContext.Provider
+            value={{ section1Ref, section2Ref, section3Ref, scrollTo }}
+        >
+            {children}
+        </ScrollContext.Provider>
+    );
+};
+
+export const useScroll = (): ScrollContextType => {
+    const context = useContext(ScrollContext);
+    if (!context) {
+        throw new Error("useScroll must be used within a ScrollProvider");
+    }
+    return context;
+};
