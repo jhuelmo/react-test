3d9159fddeb6b6f2f676d1500d9a48855d0c8f1c
Pablo Macías Moreira <pablo.macias.moreira@gmail.com>
2025-05-17 19:34:59 +0200

    Update heroSection.tsx

--- src/components/Main/HeroSection/heroSection.tsx
+++ src/components/Main/HeroSection/heroSection.tsx
@@ -3,6 +3,7 @@ import Carousel from "@/components/UI/Carousel/carousel";
 // Could use translations
 
 export default function HeroSection() {
+// use Section everywhere or rename
     return (
         <section aria-label="Eurostars hotels room images carousel">
             <Carousel></Carousel>
