b97de91ccabfae4587e4d91f87189952cec94e6c
Josu Huelmo <josu.huelmo@gmail.com>
2025-05-17 17:05:54 +0200

    - winner section title added

--- src/components/Main/WinnersSection/winnersSection.tsx
+++ src/components/Main/WinnersSection/winnersSection.tsx
@@ -1,7 +1,16 @@
 import Section from "@/components/UI/Section/section";
 import { useScroll } from "@/context/scrollContext";
+import "./winnersSection.css";
 
 export default function WinnersSection() {
     const { section3Ref } = useScroll();
-    return <Section ref={section3Ref}>WinnersSection</Section>;
+    return (
+        <Section ref={section3Ref}>
+            <div className="winners-wrapper">
+                <h3 className="winners-title">
+                    LOS GANADORES DE LAS ESTANCIAS EN HOTELES EUROSTARS
+                </h3>
+            </div>
+        </Section>
+    );
 }
