dbdce3cceb40d730bd47f03c81b6377c618e5b5f
Josu Huelmo <josu.huelmo@gmail.com>
2025-05-17 17:48:37 +0200

    card basic structure

--- src/components/UI/Card/card.tsx
+++ src/components/UI/Card/card.tsx
@@ -1,4 +1,22 @@
 import "./card.css";
+import roma from "@/assets/images/roma.jpg";
+import statue from "@/assets/images/statue.jpg";
+import suite from "@/assets/images/suite.jpg";
+
 export default function Card() {
-    return <div>Card</div>;
+    return (
+        <div className="card-wrapper">
+            <div>ROMA</div>
+            <div className="card-container">
+                <div className="card-image-container">
+                    <img className="card-image" src={roma} />
+                    <img className="card-image" src={statue} />
+                </div>
+
+                <div className="card-info">
+                    <h4>FELICIDADES!</h4>
+                </div>
+            </div>
+        </div>
+    );
 }
