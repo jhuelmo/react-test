61461a6f35159bcffea914d1cfa69574ba466c02
Pablo Macías Moreira <pablo.macias.moreira@gmail.com>
2025-05-17 19:30:50 +0200

    Update heroSection.tsx

--- src/components/Main/HeroSection/heroSection.tsx
+++ src/components/Main/HeroSection/heroSection.tsx
@@ -1,5 +1,7 @@
 import Carousel from "@/components/UI/Carousel/carousel";
 
+// Could use translations
+
 export default function HeroSection() {
     return (
         <section aria-label="Eurostars hotels room images carousel">
