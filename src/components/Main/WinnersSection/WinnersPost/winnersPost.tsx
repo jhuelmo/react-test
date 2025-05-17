a2c9f3e4eec948bd65e8dc37ebd9e7ff515c40b9
Josu Huelmo <josu.huelmo@gmail.com>
2025-05-18 00:42:37 +0200

    now the post loads the title and the date from the data

--- src/components/Main/WinnersSection/WinnersPost/winnersPost.tsx
+++ src/components/Main/WinnersSection/WinnersPost/winnersPost.tsx
@@ -4,6 +4,11 @@ import Card from "@/components/UI/Card/card";
 
 interface WinnersPost {
     post: {
+        title: string;
+        date?: {
+            startDate: string;
+            endDate: string;
+        };
         winners: string[];
         firstImage: string;
         secondImage: string;
@@ -15,12 +20,16 @@ export default function WinnersPost({ post }: WinnersPost) {
         <div className="winners-post">
             <div className="winners-post-title">
                 <Diamond color="var(--accent-color)" />
-                <h3>ROMA</h3>
-                <div className="winners-post-title-separator"></div>
-                <div className="winners-card-date">
-                    <span>28 abril</span>
-                    <span>4 mayo</span>
-                </div>
+                <h3>{post.title}</h3>
+                {post.date && (
+                    <>
+                        <div className="winners-post-title-separator"></div>
+                        <div className="winners-card-date">
+                            <span>{post.date.startDate}</span>
+                            <span>{post.date.endDate}</span>
+                        </div>
+                    </>
+                )}
                 <Diamond color="var(--accent-color)" />
             </div>
             <Card
