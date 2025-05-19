1c1aee4004850393bff9ced6c67df4c0e132aa6e
Josu Huelmo <josu.huelmo@gmail.com>
2025-05-19 02:33:16 +0200

    Created card-like styling to the form

--- src/components/Main/ParticipateSection/participateSection.tsx
+++ src/components/Main/ParticipateSection/participateSection.tsx
@@ -2,7 +2,10 @@ import Diamond from "@/components/UI/Diamond/diamond";
 import Section from "@/components/UI/Section/section";
 import { useScroll } from "@/context/scrollContext";
 
+import quizPhoto from "@/assets/roma.jpg";
+
 import "./participateSection.css";
+import Location from "@/components/UI/Icons/location";
 
 export default function ParticipateSection() {
     const { section1Ref } = useScroll();
@@ -25,13 +28,54 @@ export default function ParticipateSection() {
                         </div>
                         <h4>28 abril - 4 mayo</h4>
                     </div>
-                    <div className="participate-card">
-                        <span>
-                            Responde correctamente y gana una de las cinco
-                            estancias en hoteles Eurostars que sorteamos en
-                            nuestro destino secreto.
-                        </span>
-                    </div>
+                    <form className="participate-card-wrapper">
+                        <div className="participate-card">
+                            <span>
+                                Responde correctamente y gana una de las cinco
+                                estancias en hoteles Eurostars que sorteamos en
+                                nuestro destino secreto.
+                            </span>
+                            <div className="input-container">
+                                <input placeholder="placeholder" />
+                                <input placeholder="placeholder" />
+                            </div>
+                        </div>
+
+                        <div className="quiz-container">
+                            <div className="quiz-inner-container">
+                                <img />
+                                <span>
+                                    Sabes qué capital eurpea debemos visitar
+                                    para encontrar esta fuente?
+                                </span>
+                                <div className="quiz-answer-container">
+                                    <div className="quiz-answer">
+                                        <button className="quiz-answer-button">
+                                            <Location size="2.5rem" />
+                                        </button>
+                                        <span>ROMA</span>
+                                    </div>
+                                    <div className="quiz-answer">
+                                        <button className="quiz-answer-button">
+                                            <Location size="2.5rem" />
+                                        </button>
+                                        <span>ROMA</span>
+                                    </div>
+                                    <div className="quiz-answer">
+                                        <button className="quiz-answer-button">
+                                            <Location size="2.5rem" />
+                                        </button>
+                                        <span>ROMA</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card-photo-container">
+                            <div className="card-photo-wrapper">
+                                <img src={quizPhoto} alt="Fuente Roma" />
+                            </div>
+                        </div>
+                    </form>
                 </div>
                 <div className="participate-bot-container">Bottom</div>
             </div>
