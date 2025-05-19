2045d60272d5a183ba807b98f967ccb63106ed20
Josu Huelmo <josu.huelmo@gmail.com>
2025-05-19 14:44:53 +0200

    Social Media modified to work without background

--- src/components/UI/SocialMedia/socialMedia.tsx
+++ src/components/UI/SocialMedia/socialMedia.tsx
@@ -11,12 +11,21 @@ type SocialName =
 interface SocialIconProps {
     name: SocialName;
     size?: number;
+    onlyIcon?: boolean;
+    className?: string;
 }
 
-export default function SocialMedia({ name, size = 24 }: SocialIconProps) {
+export default function SocialMedia({
+    name,
+    size = 24,
+    onlyIcon = false,
+    className,
+}: SocialIconProps) {
     return (
         <div
-            className={`social social-${name}`}
+            className={`social social-${name} 
+                ${className}
+                ${!onlyIcon && "background"}`}
             style={{
                 width: size,
                 height: size,
